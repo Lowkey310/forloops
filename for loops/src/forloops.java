858e9468bd2b40c4e268de5409f7c206cf40c89c
tianyu.zhou <tianyu.zhou@mu-c1-17.mullen.pvt.k12.co.us>
2018-08-27 08:59:17 -0600

    forloops challenge1

--- for loops/src/forloops.java
+++ for loops/src/forloops.java
@@ -4,8 +4,35 @@ public class forloops
 
 		public static void main(String[] args)
 			{
-				
+				challenge1();
+				challenge2();
+				challenge3();
+				challenge4();
+			}
 
+		private static void challenge1()
+			{
+				for (int i=0; i<5; i++);
+					{
+						System.out.println(1-5);
+					}
+				
+			}
+		private static void challenge2()
+			{
+				
+				
+			}
+		private static void challenge3()
+			{
+				
+				
+			}
+
+		private static void challenge4()
+			{
+			    
+				
 			}
 
 	}
