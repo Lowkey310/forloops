f0b20f7aab823e99935ee65dbb4902c30610e65c
zhoutianyu <zhoutianyu@192.168.15.129>
2018-08-29 10:22:45 -0600

    forloops1

--- for loops/src/forloops.java
+++ for loops/src/forloops.java
@@ -12,7 +12,7 @@ public class forloops
 
 		private static void challenge1()
 			{
-				for (int i=1; i<=5; i++)
+				for(int i=1; i<=5; i++)
 					{
 						System.out.println(i);
 					}
@@ -20,6 +20,7 @@ public class forloops
 			}
 		private static void challenge2()
 			{
+              
 				
 				
 			}
